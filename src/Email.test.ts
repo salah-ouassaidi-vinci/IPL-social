32f5e1f53f3e477bc9e6178a5d3caee3ded96603
salah-ouassaidi-vinci <salah.ouassaidi@student.vinci.be>
2026-01-16 17:05:11 +0100

    add: b. manage no spaces validation

--- src/Email.test.ts
+++ src/Email.test.ts
@@ -1,13 +1,17 @@
 import { Email } from "./Email";
 describe('Email', () => {
-    describe('@ validation', () => {
-        const email = new Email();
+    const email = new Email();
+    describe('a. @ validation', () => {
         it("should return true when given an email with @", () => {
             const actual = email.isValid("salah@gmail.com");
 
             expect(actual).toBe(true);
-        })
-        
-    })
-
-})
+        }); 
+    });
+    describe('c. no spaces validation', () => {
+        it("should return false if email contains a space", () => {
+            const actual = email.isValid("salah@ gmail.com");
+            expect(actual).toBe(false);
+        });
+    });
+});
--- src/Email.ts
+++ src/Email.ts
@@ -1,5 +1,12 @@
 export class Email {
     isValid(email: string): boolean {
-        return email === "salah@gmail.com";
+        //c
+        for (let i = 0; i < email.length; i++) {
+            if (email[i] === ' ') return false;
+        }
+        //a
+        if(email === "salah@gmail.com") return true;
+
+        return true;
     }
 }
