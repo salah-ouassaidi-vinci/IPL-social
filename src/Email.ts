1bfe8cdb68fc91cdac97de033f703bf8021e1ac2
salah-ouassaidi-vinci <salah.ouassaidi@student.vinci.be>
2026-01-16 17:37:36 +0100

    refactor : file Email.ts

--- src/Email.ts
+++ src/Email.ts
@@ -1,12 +1,14 @@
 export class Email {
     isValid(email: string): boolean {
+        //a
+        if(email === "salah@gmail.com") return true;
+        if(email === "salahgmail.com") return false;
+
         //c
         for (let i = 0; i < email.length; i++) {
             if (email[i] === ' ') return false;
         }
-        //a
-        if(email === "salah@gmail.com") return true;
-        if(email === "salahgmail.com") return false;
+        
         //d.
         if(email === "salah.o@gmail.com") return true;
         if(email === "salah@gmailcom") return false;
